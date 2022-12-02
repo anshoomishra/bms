ddb849c195a28fa8cb78720eb0cceb476192d850
anshoo mishra <anshoomishra.in@gmail.com>
2022-12-02 15:36:05 +0530

    Models Created For Orders,Inventory,Accounts

--- account/models.py
+++ account/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser,User
+from django_countries.fields import CountryField
+from phonenumber_field import PossiblePhoneNumberField
 # Create your models here.
+
+class BillingAddress(models.Model):
+
+    '''
+    Taking BillingAddress to get help in generating Bill after
+    Putting Orders
+
+    '''
+    first_name = models.CharField(max_length=256, blank=True)
+    last_name = models.CharField(max_length=256, blank=True)
+    company_name = models.CharField(max_length=256, blank=True)
+    street_address_1 = models.CharField(max_length=256, blank=True)
+    street_address_2 = models.CharField(max_length=256, blank=True)
+    city = models.CharField(max_length=256, blank=True)
+    city_area = models.CharField(max_length=128, blank=True)
+    postal_code = models.CharField(max_length=20, blank=True)
+    country = CountryField()
+    country_area = models.CharField(max_length=128, blank=True)
+    phone = PossiblePhoneNumberField(blank=True, default="")
+
+    def __str__(self) -> str:
+        return self.street_address_1 + self.street_address_2
+
+class User(AbstractBaseUser):
+    '''
+    Custom User class For extra attributes
+
+    '''
+    first_name = models.CharField(max_length=256,blank=True)
+    last_name = models.CharField(max_length=256,blank=True)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    address  = models.ManyToManyField(BillingAddress,blank=True, related_name="user_addresses")
+    def __str__(self) -> str:
+        return self.first_name
+    
+
+
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=20)
+    quantity = models.IntegerField()  # Considering units here
+    cost_price = models.FloatField()
+    
+    
+class BakeryItem(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.ForeignKey(Ingredients)
+    created_at = models.DateTimeField()
+    modified_at = models.DateTimeField()
+    expiry_date = models.DateTimeField()
+    created_by = models.ForeignKey(User)
+    selling_price = models.FloatField()
+    is_available = models.BooleanField(default=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+
+class Inventory(models.Model):
+    bakery_item = models.ManyToManyField(BakeryItem)
+    sku = models.CharField()
+    slug = models.CharField()
+    quantity = models.IntegerField() # number of Bakery Items Available
+    
+    class Meta:
+        permissions = [("can_manage_inventory", "Can Manage Inventory")]
